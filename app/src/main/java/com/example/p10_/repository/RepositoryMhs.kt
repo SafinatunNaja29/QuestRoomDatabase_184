3b68a8130dc0dc18caca01dcf2f23e0e5e583c4f
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-17 09:31:37 +0700

    menambahkan metode untuk memanggil fungsi getAllMhs dari MahasiswaDao

--- app/src/main/java/com/example/p10_/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/p10_/repository/RepositoryMhs.kt
@@ -7,9 +7,4 @@ interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
     fun getAllMhs() : Flow<List<Mahasiswa>> //methode untuk memanggil fungsi untuk mendapatkan semua data
 
-    fun getMhs(nim: String) : Flow<Mahasiswa> //mengambil data mahasiswa berdasarkan Nim
-
-    suspend fun deleteMhs(mahasiswa: Mahasiswa) // menghapus data mahasiswa
-
-    suspend fun updateMhs(mahasiswa: Mahasiswa) //memperbarui data mahasiswa di dalam database
 }
