50026234df615273c1cdd216e16298c4dd0b593b
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-17 09:59:29 +0700

    Menambahkan alur data mahasiswa dan status loading pada HomeUiState.

--- app/src/main/java/com/example/p10_/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/p10_/ui/viewmodel/HomeMhsViewModel.kt
@@ -2,8 +2,19 @@ package com.example.p10_.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.p10_.repository.RepositoryMhs
-
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
 class HomeMhsViewModel (
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel()
+{
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map{
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
