46d6bce3a2bda561b4e0647c7800154ec9b63d1b
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-17 10:19:17 +0700

    Menambahkan pengelolaan status loading, penanganan error, dan stateIn untuk DetailUiState.

--- app/src/main/java/com/example/p10_/ui/viewmodel/DetailMahasiswaViewModel.kt
+++ app/src/main/java/com/example/p10_/ui/viewmodel/DetailMahasiswaViewModel.kt
@@ -2,11 +2,17 @@ package com.example.p10_.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.p10_.repository.RepositoryMhs
 import com.example.p10_.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
@@ -23,3 +29,22 @@ class DetailMhsViewModel (
                 isLoading = false,
             )
         }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue =
+            DetailUiState(isLoading = true)
+        )
