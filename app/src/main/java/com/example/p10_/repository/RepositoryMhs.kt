308e4ee849e2d876f69d98ee28bffe291631c8bf
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-17 09:32:15 +0700

    menambahkan metode untuk mengelola data mahasiswa

--- app/src/main/java/com/example/p10_/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/p10_/repository/RepositoryMhs.kt
@@ -7,4 +7,9 @@ interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
     fun getAllMhs() : Flow<List<Mahasiswa>> //methode untuk memanggil fungsi untuk mendapatkan semua data
 
+    fun getMhs(nim: String) : Flow<Mahasiswa> //mengambil data mahasiswa berdasarkan Nim
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa) // menghapus data mahasiswa
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa) //memperbarui data mahasiswa di dalam database
 }
