01881991154b2033ad1c3d32fdda936da1741613
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-17 09:46:25 +0700

    menambahkan object DestinasiDetail dengan parameter NIM

--- app/src/main/java/com/example/p10_/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/p10_/ui/navigation/AlamatNavigasi.kt
@@ -11,3 +11,9 @@ object DestinasiInsert : AlamatNavigasi{
 object DestinasiHome : AlamatNavigasi{
     override val route = "home"
 }
+
+object DestinasiDetail : AlamatNavigasi{
+    override val route = "detail"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
