5e14013018d2befef6f9d84fe29d9e7777934f41
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-17 10:33:59 +0700

    Menambahkan variable UpdateUiState dengan data mahasiswa berdasarkan NIM

--- app/src/main/java/com/example/p10_/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/p10_/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,11 +1,30 @@
 package com.example.p10_.ui.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.p10_.repository.RepositoryMhs
+import com.example.p10_.ui.navigation.DestinasiUpdate
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 
 class UpdateMhsViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs
-) : ViewModel()
+) : ViewModel(){
+    var updateUiState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUiState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
