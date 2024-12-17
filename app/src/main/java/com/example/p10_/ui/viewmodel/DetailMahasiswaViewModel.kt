0c0b1aaa2da455869808b35e63d24dc55d42fa83
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-17 10:14:02 +0700

    Menambahkan variable untuk DetailUiState

--- app/src/main/java/com/example/p10_/ui/viewmodel/DetailMahasiswaViewModel.kt
+++ app/src/main/java/com/example/p10_/ui/viewmodel/DetailMahasiswaViewModel.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.p10_.repository.RepositoryMhs
 import com.example.p10_.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
@@ -11,3 +14,12 @@ class DetailMhsViewModel (
 ): ViewModel()
 {
     private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
