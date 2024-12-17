5b3e738f53a7b3c4eeabf97671546c92fa2ef700
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-17 10:12:11 +0700

    Menambahkan private variable dengan validasi null

--- app/src/main/java/com/example/p10_/ui/viewmodel/DetailMahasiswaViewModel.kt
+++ app/src/main/java/com/example/p10_/ui/viewmodel/DetailMahasiswaViewModel.kt
@@ -3,8 +3,11 @@ package com.example.p10_.ui.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.p10_.repository.RepositoryMhs
+import com.example.p10_.ui.navigation.DestinasiDetail
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
 ): ViewModel()
+{
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
