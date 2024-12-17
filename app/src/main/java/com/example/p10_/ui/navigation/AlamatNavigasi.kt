b2fe04cb81d074afc00fc13d814ee21753935dc8
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-17 09:41:58 +0700

    menambahkan object DestinasiHome dengan route "home"

--- app/src/main/java/com/example/p10_/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/p10_/ui/navigation/AlamatNavigasi.kt
@@ -4,22 +4,10 @@ interface AlamatNavigasi {
     val route: String
 }
 
-object DestinasiInsert : AlamatNavigasi{   //object akan menjadi nama halaman/ menjadi pengenal halaman
+object DestinasiInsert : AlamatNavigasi{
     override val route: String = "insert_mhs"
 }
 
 object DestinasiHome : AlamatNavigasi{
     override val route = "home"
-}
-
-object DestinasiDetail : AlamatNavigasi{
-    override val route = "detail"
-    const val NIM = "nim"
-    val routesWithArg = "$route/{$NIM}"
-}
-
-object DestinasiUpdate : AlamatNavigasi{
-    override val route = "update"
-    const val NIM = "nim"
-    val routesWithArg = "$route/{${NIM}}"
 }
